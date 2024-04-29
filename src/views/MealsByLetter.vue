18117ef98ed64a28c62e30e2d58968102cb45526
gorozco9 <gorozco9@stu.southtexascollege.edu>
2024-04-28 21:08:14 -0500

    fixed navigation

--- src/views/MealsByLetter.vue
+++ src/views/MealsByLetter.vue
@@ -19,11 +19,8 @@ watch(route, () => {
 </script>
 
 <template>
-
     <div class="p-8 pb-0 text-orange-500">
-
         <h1 class="text-4xl font-bold mb-4"> Search Meals By Letter</h1>
-
     </div>
 
     <div class="flex justify-center gap-2 mt-2">
